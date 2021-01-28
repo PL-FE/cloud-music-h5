91115efb4da189a4b9c0f074116a9d9014dbea1a
pengliang <624151713@qq.com>
2021-01-28 22:44:02 +0800

    init plug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vant from 'vant'
+import router from './router'
+import store from './store'
+import 'amfe-flexible'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home-container">
+    <van-cell-group>
+      <van-cell title="单元格"
+        value="内容"></van-cell>
+      <van-cell title="单元格"
+        value="内容"></van-cell>
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
